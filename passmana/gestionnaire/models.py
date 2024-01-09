39c515ca2863b495deba390266aeca2ca98c5d97
Natsei <83393771+Natsei@users.noreply.github.com>
2024-01-09 15:46:58 +0100

    Modèles ajoutés pour les sites dans l'application gestionnaire

--- passmana/gestionnaire/models.py
+++ passmana/gestionnaire/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Site(models.Model):
+    utilisateur = models.ForeignKey(User, on_delete=models.CASCADE)
+    nom = models.CharField(max_length=100)
+    url = models.URLField()
+    identifiant = models.CharField(max_length=100)
+    mot_de_passe = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nom
