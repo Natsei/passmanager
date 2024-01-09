f356151e3228e81f1b0fc3441f17ddc99a12b1a6
Natsei <83393771+Natsei@users.noreply.github.com>
2024-01-09 16:20:23 +0100

    Mise à jour des URL pour les nouvelles vues; et suppresion de la route ''

--- passmana/gestionnaire/urls.py
+++ passmana/gestionnaire/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('sites/', views.liste_sites, name='liste_sites'),
+    path('ajouter/', views.ajouter_site, name='ajouter_site'),
+    path('modifier/<int:pk>/', views.modifier_site, name='modifier_site'),
+    path('supprimer/<int:pk>/', views.supprimer_site, name='supprimer_site'),
 ]
